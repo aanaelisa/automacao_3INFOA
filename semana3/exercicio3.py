02057b3be51c9c114c49b87a17354702447b3da1
Ana Elisa <ana4.moreira@alunos.ifsuldeminas.edu.br>
2024-03-12 16:54:02 -0300

    aluas da semana 4

--- semana3/exercicio3.py
+++ semana3/exercicio3.py
@@ -7,10 +7,25 @@ quando a matricula 0 for digitada.
 Ao final deve apresentar os números de 
 matriculas separados em 3 grupos.
 '''
-matriculas = [] #cria uma lista vazia (sem nomes)
-while True: #repete para sempre
-    matricula = input("Digite sua matrícula: ")
-    matriculas.append(matricula) #adiciona o nome na lista
-    if matricula == "0": #se o nome for igual a fim
-        print(matriculas)
-        break 
+contador = 1
+grupo1 = []
+grupo2 = []
+grupo3 = []
+
+while True:
+    matricula = int(input('Digite a matrícula: '))
+    if matricula == 0:
+        break
+    if contador == 1:
+        grupo1.append(matricula)
+    elif contador == 2:
+        grupo2.append(matricula)
+    elif contador == 3:
+        grupo3.append(matricula)
+        contador = 0
+
+    contador = contador + 1
+
+print("Grupo 1", grupo1)
+print("Grupo 2", grupo2)
+print("Grupo 3", grupo3)
---
+++ semana4/usar_funcao2.py
@@ -0,0 +1,9 @@
+#importa o arquivo inteiro
+import funcoes
+import math
+
+area = funcoes.calculateTriangleArea(50, 20)
+print("Área Tri: ", area)
+
+raizQuadrada = mathy.sqrt(100)
+print("A raiz quadrada de 100 é", raizQuadrada)
